8fde9642f8aa335a8ae54673fed5a10240eaabde
Djeck22 <126654065+Djeck22@users.noreply.github.com>
2023-03-01 10:51:41 +0300

    Initial commit

---
+++ LAB1/src/main/java/ru/geekbrains/lesson1/checkSumSign.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.lesson1;
+
+public class checkSumSign {
+    public static void main(String[] args) {
+        int a=1,b=2;
+        if (a+b>=0) {
+            System.out.println("Сумма положительная");
+        }
+        else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+}
---
+++ LAB1/src/main/java/ru/geekbrains/lesson1/compareNumbers.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.lesson1;
+
+public class compareNumbers {
+    public static void main(String[] args) {
+        int a=12,b=39;
+        if (a>b) {
+            System.out.println("a >= b");
+        }
+        if (a<b) {
+            System.out.println("a < b");
+        }
+    }
+}
